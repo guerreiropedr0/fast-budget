370c225cfc49a43ba354195bdaf19069d4debf8f
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-23 14:39:52 +0100

    Add new form for GroupsController

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,26 @@
 class GroupsController < ApplicationController
   load_and_authorize_resource
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to root_path, notice: 'Successfully created a group.'
+    else
+      flash[:alert] = 'Failed to create a group.'
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    gp = params.require(:group).permit(:name, :icon)
+    gp[:author_id] = current_user.id
+    gp
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
+
+  resources :groups, only: %i[new create]
 end
