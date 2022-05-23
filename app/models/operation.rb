3e284bbfdc3e85d5f2447ec1aa5cf293200c23c7
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-23 10:55:22 +0100

    Add Operation model specs

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,4 +1,4 @@
 class Operation < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :groups
 end
--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Operation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :example do
+    @user = User.create(name: 'Pedro Guerreiro')
+    @operation = Operation.create(name: 'Operation 1', amount: 50, author_id: @user.id)
+  end
+
+  it 'should be valid with valid attributes' do
+    expect(@operation).to be_valid
+  end
+
+  it 'should be invalid without a name' do
+    @operation.name = nil
+
+    expect(@operation).to be_invalid
+
+    @operation.name = ''
+
+    expect(@operation).to be_invalid
+  end
+
+  it 'should be invalid without a name over 100 characters' do
+    @operation.name = 'a' * 101
+
+    expect(@operation).to be_invalid
+  end
+
+  it 'should be invalid with an amount less than 1' do
+    @operation.amount = nil
+
+    expect(@operation).to be_invalid
+
+    @operation.amount = 0
+
+    expect(@operation).to be_invalid
+  end
+
+  it 'should be invalid without an author' do
+    @operation.author_id = nil
+
+    expect(@operation).to be_invalid
+  end
 end
