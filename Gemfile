856b98df4ae21bba39724023a1370bdb124c050b
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-25 09:41:29 +0100

    Add integration tests for group new view

--- Gemfile
+++ Gemfile
@@ -59,11 +59,11 @@ gem 'cancancan'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
+  gem 'capybara'
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails'
   gem 'rails-controller-testing'
   gem 'rspec-rails'
-  gem 'capybara'
   gem 'selenium-webdriver'
 end
 
---
+++ spec/system/groups/new_system_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipes new page', type: :system do
+  before :example do
+    driven_by(:rack_test)
+    @user = FactoryBot.create(:user, :confirmed)
+    sign_in @user
+    visit new_group_path
+  end
+
+  it 'should display welcoming message' do
+    expect(page).to have_content('Create Group')
+  end
+
+  it 'should display all fields' do
+    find_field 'Name'
+    find_field 'Icon'
+  end
+
+  it 'should display all links' do
+    find(:css, 'i.bi.bi-arrow-left')
+    find_button 'Save'
+  end
+
+  it 'should create a group with valid fields' do
+    fill_in 'Name', with: 'Essentials'
+    fill_in 'Icon', with: 'www.image-link.png'
+    click_button 'Save'
+
+    expect(page).to have_current_path(groups_path)
+    expect(page).to have_content('Essentials')
+    expect(page).to have_content('Successfully created a group.')
+  end
+
+  it 'should not create a recipe if one of the fields is empty' do
+    click_button('Save')
+
+    expect(page).to_not have_content('Successfully created a group.')
+    expect(page).to have_content('Failed to create a group.')
+  end
+end
